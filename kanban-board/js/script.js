4767d84685dd7e38052e99ed9da5e657bfc13d09
AkKeRise <akkerise@gmail.com>
2017-03-07 22:20:14 +0700

    Add module editJobTask

--- kanban-board/js/script.js
+++ kanban-board/js/script.js
@@ -37,10 +37,10 @@ var app = {
 		}
 	},
 
-	addJobsToList: function(type,jobName){
-		var item = '<a href="#!" class="collection-item">'+ jobName +'<span onclick="app.deleteJobInList(this)"><i class="right material-icons" style="color: #ddd">not_interested</i></span><span onclick="app.editJobInList(this)"><i class="right material-icons" style="color: #ddd">mode_edit</i></span>>';
-		$('#' + type).append(item);
-	},
+	// addJobsToList: function(type,jobName){
+	// 	var item = '<a href="#!" class="collection-item">'+ jobName +'<span onclick="app.deleteJobInList(this)"><i class="right material-icons" style="color: #ddd">not_interested</i></span><span onclick="app.editJobInList(this)"><i class="right material-icons" style="color: #ddd">mode_edit</i></span>>';
+	// 	$('#' + type).append(item);
+	// },
 
     deleteJobInList: function(span){
 		$(document).ready(function(){
@@ -64,24 +64,25 @@ var app = {
 		});
 	},
 
-	editJobInList: function(span){
-    	var itemEdit = $(span).parent().parent();
-    	var valTag_a = itemEdit.children('a').contents().filter(function() {
-        return this.nodeType == 0;
-    });
-    	var valItemEdit = itemEdit.text();
-    	console.log(valTag_a);
-    	// itemEdit.replaceWith('1');
-		$(document).ready(function () {
-
-        });
+	editJobInList: function(e,span){
+    	var itemEdit = $(span).parent();
+		itemEdit.append('<div class="input-field" id="divtask" style="padding-left: 5px;"><input id="newtask" onkeydown="app.newTasksEdit(event,this)" type="text" class="validate"><label for="newtask">New Task</label></div>');
 	},
+
+    newTasksEdit: function (e,input) {
+		var evenNewTasksEdit = window.event || e;
+		var jobNewTaskEdit = $(input).val();
+		var parentNewTaskEdit = $(input).parent().parent();
+		if(evenNewTasksEdit.keyCode == 13 && jobNewTaskEdit !== ''){
+            parentNewTaskEdit.after('<a href="#!" class="collection-item">'+ jobNewTaskEdit + '<span onclick="app.deleteJobInList(this)"><i class="right material-icons" style="color: #ddd">not_interested</i></span><span onclick="app.editJobInList(this)"><i class="right material-icons" style="color: #ddd">mode_edit</i></span>');
+            parentNewTaskEdit.remove();
+		}
+    },
 
 	countTasks: function(){
 		var count = $('.count');
 		count.each(function(i) {
 			var bindCount = $(this).parent().parent().next().find('a').length - 1;
-			console.log(this.parent());
 		});
 	}
 };

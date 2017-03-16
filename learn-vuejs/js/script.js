0f16f34c3714a80f1ebad51d85b5c5464b0c2677
AkKe Rise <AkKe Rise>
2017-03-17 03:07:37 +0700

    Add

--- learn-vuejs/js/script.js
+++ learn-vuejs/js/script.js
@@ -5,26 +5,70 @@ new Vue({
     title: "Hello World!",
     finishedLink: '<a href="http://google.com">Google.com</a>',
     counter: 0,
-      x: 0,
-      y: 0
+    x: 0,
+    y: 0,
+    name: 'Thanh'
   },
   methods: {
   	sayHello: function(){
-    	this.title = "Hello";
-    	return this.title;
-    },
-    changeTitle: function(e){
-    	this.title = e.target.value;
-    },
-    increase: function(step , event){
-    	this.counter += 2;
-    },
-      updateCoordinates: function (event) {
-         this.x = event.clientX;
-         this.y = event.clientY;
-      },
-      dummy: function(event){
-        event.stopPropagation();
+     this.title = "Hello";
+     return this.title;
+   },
+   changeTitle: function(e){
+     this.title = e.target.value;
+   },
+   increase: function(step , event){
+     this.counter += 2;
+   },
+   updateCoordinates: function (event) {
+     this.x = event.clientX;
+     this.y = event.clientY;
+   },
+   dummy: function(event){
+    event.stopPropagation();
+  },
+
+}
+});
+
+new Vue({
+  el: '#id2',
+  data: function(){
+      return {
+          counter: 0,
+          secondCounter: 0,
+          tt: 'http://www.gem.com'
       }
+  },
+  computed: {
+    output: function(){
+      console.log('Computed')
+      return this.counter > 5 ? 'Greater 5' : 'Smaller than 5';
+    }
+  },
+    watch: {
+        counter: function(value){
+            var vm = this;
+            setTimeout(function(){
+                vm.counter = 0;
+            },2000);
+        }
+    },
+  methods: {
+    increase: function(){
+      this.counter++;
+      this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    },
+    decrease: function(){
+      this.counter--;
+      this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    },
+    result: function() {
+      console.log('Methods');
+      return this.counter > 5 ? 'Greater 5' : 'Smaller than 5';
+    },
+    gem: function(){
+        this.tt = 'http://www.ccc.com';
+    }
   }
 });

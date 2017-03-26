23fe142654b1f16177222299c26c80e3d031bdf2
AkKeRise <akkerise@gmail.com>
2017-03-26 18:21:30 +0700

    Add learn-vuejs/ : style2.css script2.js index2.js

---
+++ learn-vuejs/js/script2.js
@@ -0,0 +1,15 @@
+/**
+ * Created by AkKe on 3/26/2017.
+ */
+new Vue({
+    el: '#app',
+    data: {
+        // attachRed: false
+        isActive: true,
+        hasError: true,
+        classObject: {
+            active: true,
+            'text-danger': true
+        }
+    }
+});

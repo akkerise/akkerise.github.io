83ee782cdd3d060e173d014620cf0f3aef7a0d5b
AkKeRise <akkerise@gmail.com>
2017-04-18 23:50:59 +0700

    Add vue-devtools/

--- learn-vuejs/js/v-for.js
+++ learn-vuejs/js/v-for.js
@@ -1,3 +1,13 @@
 /**
  * Created by AkKe on 4/18/2017.
  */
+new Vue({
+    el: '#app',
+    data: {
+        ingredients: ['meat', 'fruit', 'cookie'],
+        persons: [
+            {name: 'Max', age: 27, color: 'red'},
+            {name: 'Anna', age: 'unknow', color: 'blue'}
+        ]
+    }
+});
